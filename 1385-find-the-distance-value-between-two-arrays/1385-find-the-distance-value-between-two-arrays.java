e58215da9a504a7c494570dadd716db437422f8b
Ankur Gupta <31045221+ankurg132@users.noreply.github.com>
2022-05-03 18:46:56 +0530

    Time: 5 ms (63.58%), Space: 44.4 MB (51.46%) - LeetHub

---
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        Arrays.sort(arr2);
+        int ans = 0;
+        // we find 2 elements closest in arr2 to the choosen element in arr1
+        for(int i=0;i<arr1.length;i++){
+            int index = Arrays.binarySearch(arr2, 0, arr2.length, arr1[i]);
+            //if index is not found, the function returns the negative position where the element can be inserted, the next line converts this into positive index
+            if(index<0){
+             index = -(index+1);   
+            }
+            boolean isShort = false;
+            if(index < arr2.length && Math.abs(arr2[index]-arr1[i])<=d){
+                isShort = true;
+            }
+            if(index != 0 && Math.abs(arr2[index-1] - arr1[i]) <= d){
+                isShort = true;
+            }
+            if(!isShort){
+                ans++;
+            }
+            
+        }
+        return ans;
+    }
+}
