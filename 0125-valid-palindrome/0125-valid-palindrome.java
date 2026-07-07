1c3d8ec00c074a001600607671b1cd9de79530fd
Ankur Gupta <31045221+ankurg132@users.noreply.github.com>
2026-07-07 16:34:14 +0530

    Time: 9 ms (36.76%), Space: 44.9 MB (44.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -8,7 +8,7 @@ class Solution {
             }
         }
         String cleaned = sb.toString();
-        for(int i=0;i < cleaned.length(); i++){
+        for(int i=0;i < cleaned.length() / 2; i++){
             if(!(cleaned.charAt(i) == cleaned.charAt(cleaned.length() - 1 - i))){
                 return false;
             }
