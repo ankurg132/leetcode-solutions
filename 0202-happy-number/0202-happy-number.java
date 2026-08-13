396ce8cf657770a13b22c9eb5d67f68aa7db9db0
Ankur Gupta <31045221+ankurg132@users.noreply.github.com>
2026-08-14 00:26:29 +0530

    Time: 1 ms (77.51%), Space: 42.4 MB (67.21%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,24 +1,20 @@
 class Solution {
     public boolean isHappy(int n) {
-        int fast = n;
-        int slow = n;
+        int s = n;
+        int f = n;
         do{
-            fast = findNext(findNext(fast));
-            slow = findNext(slow);
-        }while(fast != slow);
-        //fast == slow in 2 conditions: both equal to 1, or both are cycle and stops at a fixed point.
-        if(fast == 1){
-            return true;
-        }
-        return false;
+            s = findNext(s);
+            f = findNext(findNext(f));
+        }while(s!=f);
+        return s == 1;
     }
     public int findNext(int n){
-        int result = 0;
-        while(n != 0){
-            int rem = n % 10;
-            result += rem*rem;
+        int answer = 0;
+        while(n!=0){
+            int rem = n%10;
+            answer += rem*rem;
             n = n/10;
         }
-        return result;
+        return answer;
     }
 }
