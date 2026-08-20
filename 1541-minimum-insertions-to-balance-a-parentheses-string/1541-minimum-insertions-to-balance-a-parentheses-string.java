266fd9fa9f1efb1539a16b56a76b07565fb9fac6
Ankur Gupta <31045221+ankurg132@users.noreply.github.com>
2026-08-20 18:04:54 +0530

    Time: 40 ms (5.18%), Space: 47.7 MB (25.18%) - LeetHub

--- 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
+++ 1541-minimum-insertions-to-balance-a-parentheses-string/1541-minimum-insertions-to-balance-a-parentheses-string.java
@@ -10,10 +10,9 @@ class Solution {
                 if(i+1 < s.length() && s.charAt(i+1) == ')'){
                     i++;
                     //)) but no starting (, needs to be appended.
-                    if(stack.isEmpty() || stack.peek() != '('){
+                    if(stack.isEmpty()){
                         answer += 1;
-                    }
-                    if(!stack.isEmpty()){
+                    }else{
                         stack.pop();
                     }
                 }else {
@@ -22,8 +21,6 @@ class Solution {
                         answer += 2;
                     }else{
                         answer += 1;
-                    }
-                    if(!stack.isEmpty()){
                         stack.pop();
                     }
                 }
